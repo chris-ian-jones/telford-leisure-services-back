93287bf9d905b291dcb2dbd2b2ab96a794d8eca9
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-05-24 06:06:03 +0100

    TLS: refactored config module

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,13 +1,38 @@
-import { Module } from '@nestjs/common';
-import { ConfigService } from './config.service';
+// import { Module } from '@nestjs/common';
+// import { ConfigService } from './config.service';
 
-@Module({
-  providers: [
-    {
-      provide: ConfigService,
-      useValue: new ConfigService(".env"),
-    },
-  ],
-  exports: [ConfigService],
-})
-export class ConfigModule {}
+// @Module({
+//   providers: [
+//     {
+//       provide: ConfigService,
+//       useValue: new ConfigService(".env"),
+//     },
+//   ],
+//   exports: [ConfigService],
+// })
+// export class ConfigModule {}
+
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigService } from './config.service';
+import { CONFIG_OPTIONS } from './constants';
+
+export interface ConfigModuleOptions {
+  folder: string;
+}
+
+@Module({})
+export class ConfigModule {
+  static register(options: ConfigModuleOptions): DynamicModule {
+    return {
+      module: ConfigModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        ConfigService,
+      ],
+      exports: [ConfigService],
+    };
+  }
+}
